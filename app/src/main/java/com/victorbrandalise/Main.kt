2f8f6ca60b080102066d44ae0a3e2958ecf6f04d
Harystolho <haristolhogamer@gmail.com>
2021-05-31 21:24:23 -0300

    Add coroutines

--- app/src/main/java/com/victorbrandalise/Main.kt
+++ app/src/main/java/com/victorbrandalise/Main.kt
@@ -4,6 +4,8 @@ import io.grpc.android.AndroidChannelBuilder
 import java.util.concurrent.TimeUnit
 
 fun main() {
+    val scope = CoroutineScope
+
     val channel = AndroidChannelBuilder
         .forAddress("127.0.0.1", 9090)
         .build()
@@ -21,5 +23,15 @@ fun main() {
 
     channel.getState(false)
 
+    val book = BookOuterClass.Book.newBuilder()
+        .setId(1)
+        .setTitle("gRPC In-Depth")
+        .build()
 
+    val createBookRequest = BookOuterClass.CreateBookRequest.newBuilder()
+        .setBook(book)
+        .build()
+
+    val stub = BookstoreGrpcKt.BookstoreCoroutineStub(channel)
+    val a = stub.createBook(createBookRequest)
 }
