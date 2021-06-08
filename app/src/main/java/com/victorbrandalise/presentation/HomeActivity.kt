5861e7975d107ded0f83ea00fb0f6c915038dce1
Harystolho <haristolhogamer@gmail.com>
2021-06-07 21:17:30 -0300

    Create empty activities

--- app/src/main/java/com/victorbrandalise/presentation/HomeActivity.kt
+++ app/src/main/java/com/victorbrandalise/presentation/HomeActivity.kt
@@ -2,12 +2,17 @@ package com.victorbrandalise.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.victorbrandalise.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
 }
--- app/src/main/java/com/victorbrandalise/presentation/LockscreenActivity.kt
+++ app/src/main/java/com/victorbrandalise/presentation/LockscreenActivity.kt
@@ -1,8 +1,17 @@
 package com.victorbrandalise.presentation
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.victorbrandalise.databinding.ActivityLockscreenBinding
 
 class LockscreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLockscreenBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLockscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 
 }
