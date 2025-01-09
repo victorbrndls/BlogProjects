b1f985bedfb6f59a712b67989a064122602b8e71
Victor B <haristolhogamer@gmail.com>
2025-01-09 10:44:46 -0300

    Get CameraX preview working

--- app/src/main/java/com/victorbrandalise/App.kt
+++ app/src/main/java/com/victorbrandalise/App.kt
@@ -1,142 +1,99 @@
 package com.victorbrandalise
 
-import androidx.compose.animation.AnimatedContent
-import androidx.compose.animation.AnimatedContentScope
-import androidx.compose.animation.ExperimentalSharedTransitionApi
-import androidx.compose.animation.SharedTransitionLayout
-import androidx.compose.animation.SharedTransitionScope
-import androidx.compose.foundation.clickable
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
+import androidx.camera.core.Preview as CameraPreview
 
-@OptIn(ExperimentalSharedTransitionApi::class)
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun App() {
     MaterialTheme {
         Surface(
             modifier = Modifier.fillMaxSize()
         ) {
-            var showDetails by remember { mutableStateOf(false) }
+            val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
 
-            SharedTransitionLayout {
-                AnimatedContent(
-                    showDetails,
-                    label = "basic_transition"
-                ) { targetState ->
-                    Box(Modifier.fillMaxSize()) {
-                        if (!targetState) {
-                            LessonCard(
-                                { showDetails = true },
-                                this@SharedTransitionLayout,
-                                this@AnimatedContent
-                            )
-                        } else {
-                            LessonDetail(
-                                { showDetails = false },
-                                this@SharedTransitionLayout,
-                                this@AnimatedContent
-                            )
-                        }
-                    }
+            val requestPermissionLauncher = rememberLauncherForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { _ -> } // We can ignore this because [cameraPermissionState] updates
+
+            LaunchedEffect(cameraPermissionState) {
+                if (
+                    !cameraPermissionState.status.isGranted &&
+                    cameraPermissionState.status.shouldShowRationale
+                ) {
+                    // Show rationale if needed
+                } else {
+                    requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
                 }
             }
-        }
-    }
-}
 
-@OptIn(ExperimentalSharedTransitionApi::class)
-@Composable
-private fun LessonCard(
-    showDetails: () -> Unit,
-    sharedTransitionScope: SharedTransitionScope,
-    animatedContentScope: AnimatedContentScope
-) {
-    with(sharedTransitionScope) {
-        Card(
-            modifier = Modifier
-                .padding(12.dp)
-                .clickable { showDetails() }
-        ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp)
-            ) {
-                Text(
-                    text = "Lesson 1",
-                    fontSize = 25.sp,
-                    modifier = Modifier
-                        .sharedBounds(
-                            rememberSharedContentState(key = "title"),
-                            animatedVisibilityScope = animatedContentScope
-                        )
-                )
-                Text(
-                    text = "Let's learn about Compose",
-                    fontSize = 14.sp,
-                    modifier = Modifier.sharedBounds(
-                        rememberSharedContentState(key = "subtitle"),
-                        animatedVisibilityScope = animatedContentScope
-                    )
-                )
+            if (cameraPermissionState.status.isGranted) {
+                CameraPreview()
+            } else {
+                NoCameraPermission()
             }
         }
     }
 }
 
-@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
-private fun LessonDetail(
-    hideDetails: () -> Unit,
-    sharedTransitionScope: SharedTransitionScope,
-    animatedContentScope: AnimatedContentScope
-) {
-    with(sharedTransitionScope) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier
-        ) {
-            Text(
-                text = "Lesson 1",
-                fontSize = 35.sp,
-                modifier = Modifier
-                    .clickable { hideDetails() }
-                    .sharedBounds(
-                        rememberSharedContentState(key = "title"),
-                        animatedVisibilityScope = animatedContentScope
-                    )
-                    .padding(vertical = 16.dp)
-            )
-            Text(
-                text = "Jetpack Compose is a modern toolkit designed to simplify and accelerate UI development on Android. As a declarative UI framework, it allows developers to build responsive and dynamic user interfaces with less code and more intuitive design principles. Here's an in-depth look at Compose and how it can help you create better layouts",
-                fontSize = 14.sp,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .sharedBounds(
-                        rememberSharedContentState(key = "subtitle"),
-                        animatedVisibilityScope = animatedContentScope
-                    )
-                    .padding(12.dp)
-            )
-        }
+fun CameraPreview() {
+    val lensFacing = CameraSelector.LENS_FACING_BACK
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+
+    // Preview use case
+    val preview = CameraPreview.Builder().build()
+    val previewView = remember { PreviewView(context) }
+
+    // Image capture use case
+    val imageCapture = remember { ImageCapture.Builder().build() }
+
+    LaunchedEffect(lensFacing) {
+        val cameraProvider = ProcessCameraProvider.getInstance(context).get()
+        val cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview, imageCapture)
+        preview.surfaceProvider = previewView.surfaceProvider
     }
+    Box(
+        contentAlignment = Alignment.BottomCenter,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        AndroidView(
+            { previewView },
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun NoCameraPermission() {
+    Text("You need to give camera permission")
 }
 
 @Preview(showSystemUi = true)
